84c56dfffb27a273523dd431487c36eecc44555a
sirokujira <toru753@gmail.com>
2016-06-21 11:10:08 +0900

    modified

--- external-project-macros.cmake
+++ external-project-macros.cmake
@@ -70,24 +70,24 @@ macro(compile_vtk)
       -DCMAKE_BUILD_TYPE:STRING=${build_type}
       -DBUILD_SHARED_LIBS:BOOL=ON
       -DBUILD_TESTING:BOOL=OFF
-      -DVTK_ANDROID_BUILD                OFF
-      -DVTK_EXTRA_COMPILER_WARNINGS      OFF
-      -DVTK_Group_Imaging                OFF
-      -DVTK_Group_MPI                    OFF
-      -DVTK_Group_Qt                     OFF
-      -DVTK_Group_Rendering              OFF
-      -DVTK_Group_StandAlone             ON
-      -DVTK_Group_Tk                     OFF
-      -DVTK_Group_Views                  OFF
-      -DVTK_Group_Web                    OFF
-      -DVTK_IOS_BUILD                    OFF
-      -DVTK_PYTHON_VERSION               2
-      -DVTK_RENDERING_BACKEND            OpenGL
-      -DVTK_SMP_IMPLEMENTATION_TYPE      Sequential
-      -DVTK_USE_LARGE_DATA               OFF
-      -DVTK_WRAP_JAVA                    OFF
-      -DVTK_WRAP_PYTHON                  OFF
-      -DVTK_WRAP_TCL                     OFF
+      -DVTK_ANDROID_BUILD:BOOL=OFF
+      -DVTK_EXTRA_COMPILER_WARNINGS:BOOL=OFF
+      -DVTK_Group_Imaging:BOOL=OFF
+      -DVTK_Group_MPI:BOOL=OFF
+      -DVTK_Group_Qt:BOOL=OFF
+      -DVTK_Group_Rendering:BOOL=OFF
+      -DVTK_Group_StandAlone:BOOL=ON
+      -DVTK_Group_Tk:BOOL=OFF
+      -DVTK_Group_Views:BOOL=OFF
+      -DVTK_Group_Web:BOOL=OFF
+      -DVTK_IOS_BUILD:BOOL=OFF
+      -DVTK_PYTHON_VERSION:STRING=2
+      -DVTK_RENDERING_BACKEND:STRING=OpenGL
+      -DVTK_SMP_IMPLEMENTATION_TYPE:STRING=Sequential
+      -DVTK_USE_LARGE_DATA:BOOL=OFF
+      -DVTK_WRAP_JAVA:BOOL=OFF
+      -DVTK_WRAP_PYTHON:BOOL=OFF
+      -DVTK_WRAP_TCL:BOOL=OFF
       find_package(OpenGL REQUIRED)
       ${vtk_module_defaults}
   )
