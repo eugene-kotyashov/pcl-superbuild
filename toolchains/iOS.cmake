1784dc1fb7f4f049a20734e3f5a344bae7fb52d3
sirokujira <toru753@gmail.com>
2016-10-31 09:49:55 +0900

    cmake changed

--- toolchains/iOS.cmake
+++ toolchains/iOS.cmake
@@ -43,7 +43,7 @@ set (IOS True)
 # set(CMAKE_IOS_DEVELOPER_ROOT, "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS9.1.sdk")
 #                                 Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Dev.
 set(CMAKE_IOS_DEVELOPER_ROOT, "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/")
-# set(CMAKE_IOS_SDK_ROOT, "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS9.1.sdk"))
+set(CMAKE_IOS_SDK_ROOT, "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS9.1.sdk"))
 
 # Required as of cmake 2.8.10
 set (CMAKE_OSX_DEPLOYMENT_TARGET "" CACHE STRING "Force unset of the deployment target for iOS" FORCE)
