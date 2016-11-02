d56e8c03fdb3860d5f6f8f54533274cb276537c2
sirokujira <toru753@gmail.com>
2016-11-02 11:14:09 +0900

    cmake test

--- external-project-macros.cmake
+++ external-project-macros.cmake
@@ -164,7 +164,9 @@ macro(fetch_flann)
     flann-fetch
     SOURCE_DIR ${source_prefix}/flann
     GIT_REPOSITORY git://github.com/mariusmuja/flann
+    # old
     # GIT_TAG cee08ec38a8df7bc70397f10a4d30b9b33518bb4
+    # SampleBuild NG
     # GIT_TAG 1.8.4
     GIT_TAG 1.8.5
     CONFIGURE_COMMAND ""
