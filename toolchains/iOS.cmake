b2f59b11e34aac5233211b6bb183b2ea7cf830be
sirokujira <toru753@gmail.com>
2016-11-17 12:25:30 +0900

    test

--- toolchains/iOS.cmake
+++ toolchains/iOS.cmake
@@ -202,7 +202,9 @@ set (CMAKE_OSX_SYSROOT ${CMAKE_IOS_SDK_ROOT} CACHE PATH "Sysroot used for iOS su
 if (${IOS_PLATFORM} STREQUAL "OS")
 #     set (IOS_ARCH armv7 armv7s arm64)
 # http://d.hatena.ne.jp/feb-hare/comment/20111013/1318506885
-      set (IOS_ARCH "armv7;armv7s;arm64")
+#       set (IOS_ARCH "armv7;armv7s;arm64")
+# iOS10 SDK not support armv7
+      set (IOS_ARCH "armv7s;arm64")
 #     set (IOS_ARCH arm64)
 elseif (${IOS_PLATFORM} STREQUAL "SIMULATOR")
     set (IOS_ARCH i386)
