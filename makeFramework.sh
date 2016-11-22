7561f285b12af4918e99e00d547f7a00b9a335af
sirokujira <toru753@gmail.com>
2016-11-22 12:06:24 +0900

    test

--- makeFramework.sh
+++ makeFramework.sh
@@ -15,9 +15,9 @@ make_pcl_framework ()
 
   # args -> version
   # version 1.7
-  # pcl_header_dir=$install/pcl-ios-device/include/pcl-1.7
+  pcl_header_dir=$install/pcl-ios-device/include/pcl-1.7
   # version 1.8
-  pcl_header_dir=$install/pcl-ios-device/include/pcl-1.8
+  # pcl_header_dir=$install/pcl-ios-device/include/pcl-1.8
 
   pcl_framework=$install/frameworks/pcl.framework
   mkdir -p $pcl_framework
@@ -26,12 +26,11 @@ make_pcl_framework ()
   cp -r $pcl_header_dir/* $pcl_framework/Headers/
 
   libtool -static -o $pcl_framework/pcl_device $pcl_device_libs
-  libtool -static -o $pcl_framework/pcl_sim $pcl_sim_libs
-  lipo -create $pcl_framework/pcl_device $pcl_framework/pcl_sim -output $pcl_framework/pcl
-  #lipo -create $pcl_framework/pcl_device -output $pcl_framework/pcl
+  lipo -create $pcl_framework/pcl_device -output $pcl_framework/pcl
+  # libtool -static -o $pcl_framework/pcl_sim $pcl_sim_libs
+  # lipo -create $pcl_framework/pcl_device $pcl_framework/pcl_sim -output $pcl_framework/pcl
   rm $pcl_framework/pcl_*
 }
-
 
 
 #------------------------------------------------------------------------------
