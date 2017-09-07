082076ac70a84ba129db94675565a5c3ff51f31b
sirokujira <toru753@gmail.com>
2017-09-07 14:50:06 +0900

    find_host_program move another cmake travis ci virtual memory add

--- external-project-macros.cmake
+++ external-project-macros.cmake
@@ -464,28 +464,6 @@ macro(crosscompile_pcl tag)
   force_build(${proj})
 endmacro()
 
-# macro to find programs on the host OS 
-# use cmake FindCUDA
-# https://gitlab.kitware.com/cmake/cmake/issues/16509
-macro(find_host_program)
- set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER ) 
- set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER ) 
- set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER ) 
- if( CMAKE_HOST_WIN32 ) 
-  SET( WIN32 1 ) 
-  SET( UNIX ) 
- elseif( CMAKE_HOST_APPLE ) 
-  SET( APPLE 1 ) 
-  SET( UNIX ) 
- endif() 
- find_program( ${ARGN} ) 
- SET( WIN32 ) 
- SET( APPLE ) 
- SET( UNIX 1 ) 
- set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY ) 
- set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY ) 
- set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY ) 
-endmacro() 
 
 
 macro(create_pcl_framework)
