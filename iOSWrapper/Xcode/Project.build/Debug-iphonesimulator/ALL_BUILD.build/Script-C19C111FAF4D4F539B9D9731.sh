292763ea468aa3c8069b9422cee0ed6b90bac8a8
sirokujira <toru753@gmail.com>
2018-02-05 23:42:16 +0900

    reomve xcode folder

--- iOSWrapper/Xcode/Project.build/Debug-iphonesimulator/ALL_BUILD.build/Script-C19C111FAF4D4F539B9D9731.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/T_O/pcl-superbuild/iOSWrapper/Xcode -f /Users/T_O/pcl-superbuild/iOSWrapper/Xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- iOSWrapper/Xcode/Project.build/Debug-iphonesimulator/ZERO_CHECK.build/Script-EAE67ED344C14DAC85F95FBE.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/T_O/pcl-superbuild/iOSWrapper/Xcode -f /Users/T_O/pcl-superbuild/iOSWrapper/Xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
