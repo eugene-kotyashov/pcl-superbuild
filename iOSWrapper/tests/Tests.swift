51ca829b5182f54538dc6203a34158988ea05519
sirokujira <toru753@gmail.com>
2018-03-06 09:42:54 +0900

    modified iOSWrapper(test)

--- iOSWrapper/tests/Tests.swift
+++ iOSWrapper/tests/Tests.swift
@@ -19,7 +19,8 @@ class Tests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let i = libPointCloudLibrary.Hogehoge();
+        libPointCloudLibrary.Load("pointcloud.pcd");
+        let i = libPointCloudLibrary.GetPointCloudData();
         XCTAssert(i);
     }
 
@@ -36,89 +37,17 @@ class Tests: XCTestCase {
      * 結果を戻り値で返す。
      */
     func testReturnVelue() {
-        let manager = NetworkManager()
-        let userData = manager.userData()
+        let manager = libPointCloudLibrary()
+        let pointData = manager.GetPointCloudData()
 
         // 戻り値を確認する場合は、素直に比較する。
-        XCTAssertNotNil(userData)
+        XCTAssertNotNil(pointData)
 
-        XCTAssertEqual(userData.username, "TEST1234")
-        XCTAssertEqual(userData.uuid, "CAD34831-E763-45A9-8BA2-31991DCB682B")
-        XCTAssertEqual(userData.rank, 1)
+        XCTAssertEqual(pointData.x, "TEST1234")
+        XCTAssertEqual(pointData.y, "CAD34831-E763-45A9-8BA2-31991DCB682B")
+        XCTAssertEqual(pointData.z, 1)
 
-        XCTAssertNotNil(userData.latestAccessDate)
-    }
-
-    // MARK: - Asynchronous(Delegate)
-
-    /*!
-     * 結果を Delegate で返す。
-     */
-    func testCallApiDelegate() {
-        // 非同期処理の完了を監視するオブジェクトを作成
-        // 別メソッドになるためメンバ変数を用意
-        self.callApiExpectation = self.expectation(description: "CallApiDelegate")
-
-        let manager = NetworkManager()
-        manager.delegate = self
-        manager.callApi(command: .Update, parameters: nil, completionHandler: nil)
-
-        // 指定秒数待つ
-        self.waitForExpectations(timeout: 10, handler: nil)
-    }
-
-    func callBackApi(result: [NetworkManager.ApiResutKeys : Any]) {
-        // 非同期処理の監視を終了
-        self.callApiExpectation?.fulfill()
-        // 結果を確認
-        XCTAssertNotNil(result)
-    }
-
-    // MARK: - Asynchronous(Blocks)
-
-    /*!
-     * 非同期処理で結果を Blocks で返す。
-     */
-    func testImageDownlaodBlocks() {
-        // 非同期処理の完了を監視するオブジェクトを作成
-        let expectation = self.expectation(description: "CallApiBlocks")
-
-        let manager = NetworkManager()
-        manager.delegate = self
-        manager.callApi(command: .Update, parameters: nil) { (result, apiResult, error) in
-            // 非同期処理の監視を終了
-            expectation.fulfill()
-            // 結果を確認
-            XCTAssertTrue(result)
-        }
-
-        // 指定秒数待つ
-        self.waitForExpectations(timeout: 10, handler: nil)
-    }
-
-    // MARK: - Asynchronous(Notification)
-
-    /*!
-     * 結果を Notification で返す。
-     */
-    func testCallApiNotification() {
-        // 通知を監視する
-        self.expectation(forNotification: NetworkManager.ApiCallbackNotification.rawValue, object: nil) { (notification) -> Bool in
-            // 結果を確認
-            XCTAssertNotNil(notification.object)
-            let apiResult = notification.object as! [NetworkManager.ApiResutKeys: AnyObject]
-            XCTAssertTrue(apiResult[.Status] as! Bool)
-            XCTAssertNil(apiResult[.Error])
-
-            // 通知の監視を終了
-            return true
-        }
-
-        let manager = NetworkManager()
-        manager.callApi(command: .Update, parameters: nil, completionHandler: nil)
-
-        // 指定秒数待つ
-        self.waitForExpectations(timeout: 10, handler: nil)
+        XCTAssertNotNil(pointData.latestAccessDate)
     }
 }
 
