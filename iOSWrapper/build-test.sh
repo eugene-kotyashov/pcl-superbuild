9afef5a2af6074b02c3b25d40acae25662b4bc62
sirokujira <toru753@gmail.com>
2018-03-15 18:45:20 +0900

    modified tests

--- iOSWrapper/build-test.sh
+++ iOSWrapper/build-test.sh
@@ -1,2 +1,2 @@
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/iOS_xcode.cmake -DIOS_PLATFORM=SIMULATOR64 -H. -Bbuild.test -GXcode
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchains/iOS_xcode.cmake -DIOS_PLATFORM=SIMULATOR64 -DIOS_TESTS=1 -H. -Bbuild.test -GXcode
 cmake --build build.test/ --config Release
